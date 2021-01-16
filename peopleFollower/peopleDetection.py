279c9590072b1341006e91f12ffdf7b7d6302d9f
Fox <filipiukjarek@gmail.com>
2021-01-16 20:50:41 +0100

    detectMultiscale() tested and too slow

--- peopleFollower/peopleDetection.py
+++ peopleFollower/peopleDetection.py
@@ -3,20 +3,25 @@ import numpy as np
 import imutils
 import time
 import easygopigo3
-import imutils
+import utils
+
 from picamera.array import PiRGBArray
 from picamera import PiCamera 
-from distanceFinder import findCameraDistance
+#from utils import findCameraDistance
 from centroidTracker import CentroidTracker
 from imutils.object_detection import non_max_suppression
 
 GPG = easygopigo3.EasyGoPiGo3() # Create an instance of the GoPiGo3 class. GPG will be the GoPiGo3 object.
 GPG.set_speed(300)
 
+IMAGE_WIDTH = 384
+IMAGE_HEIGHT = 288
+STEER_TRESHOLD = 20
+
 # Initialize PiCamera
 camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 20
+camera.resolution = (IMAGE_WIDTH, IMAGE_HEIGHT)
+camera.framerate = 10
 camera.rotation = 180
 
 #initialize peopleTracker
@@ -26,27 +31,31 @@ peopleTracker = CentroidTracker()
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-IMAGE_WIDTH = 640
-IMAGE_HEIGHT = 480
-STEER_TRESHOLD = 20
+
 
 rawCapture = PiRGBArray(camera, size=(IMAGE_WIDTH, IMAGE_HEIGHT))
 
-prevArea = 0;
-currentArea =0
+# movement controller parameters
+error = 0.0
+setpoint = int(IMAGE_WIDTH / 2)
 
 #loop through frames continuously
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    startTime = time.time()
+
     image = frame.array
 
     image = imutils.resize(image, width=min(400, image.shape[1]))
 
+
     # detect people in the image
-    (rects, weights) = hog.detectMultiScale(image, winStride=(6, 6),
-    padding=(8, 8), scale=1.05)
+    # (rects, weights) = hog.detectMultiScale(image, winStride=(6, 6),
+    # padding=(8, 8), scale=1.05)
+    (rects, weights) = hog.detectMultiScale(image, winStride=(10, 10),
+    padding=(8, 8), scale=1.1)
 
     rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-    pick = non_max_suppression(rects, probs=None, overlapThresh=0.80)
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 
 
     # draw the final bounding boxes
@@ -56,16 +65,16 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         # width = endX - startX
         # height = endY - startY
         # 
-    objects = peopleTracker.update(pick)
+    # objects = peopleTracker.update(pick)
 
-    if bool(objects):
-        for (objectID, centroid) in objects.items():
-            # draw both the ID of the object and the centroid of the
-            # object on the output image
-            text = "ID {}".format(objectID)
-            cv2.putText(image, text, (centroid[0] - 10, centroid[1] - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            cv2.circle(image, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+    # if bool(objects):
+    #     for (objectID, centroid) in objects.items():
+    #         # draw both the ID of the object and the centroid of the
+    #         # object on the output image
+    #         text = "ID {}".format(objectID)
+    #         cv2.putText(image, text, (centroid[0] - 10, centroid[1] - 10),
+    #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    #         cv2.circle(image, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
 
         ###### HANDLE ROBOT MOVEMENT_ START #####
 
@@ -86,7 +95,9 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 
     cv2.imshow("outputImage", image)
+    endTime = time.time()
 
+    print("image processing time: ", endTime - startTime)
     # Exit if 'esc' is clicked
     # cleanup hardware
     key = cv2.waitKey(1)
