eee3cafe12b69f2818b72e17419252b796661311
Fox <filipiukjarek@gmail.com>
2021-01-17 13:51:36 +0100

    threshold changed to recognize and follow bright pink color

--- peopleFollower/peopleDetection.py
+++ peopleFollower/peopleDetection.py
@@ -3,81 +3,150 @@ import numpy as np
 import imutils
 import time
 import easygopigo3
-import utils
+import config as cfg
 
+from threading import Thread
+from collections import OrderedDict
 from picamera.array import PiRGBArray
 from picamera import PiCamera 
-#from utils import findCameraDistance
+from utils import findCameraDistance, positionControl_PID, findCameraDistance, rectArea
 from centroidTracker import CentroidTracker
 from imutils.object_detection import non_max_suppression
 
-GPG = easygopigo3.EasyGoPiGo3() # Create an instance of the GoPiGo3 class. GPG will be the GoPiGo3 object.
-GPG.set_speed(300)
-
-IMAGE_WIDTH = 384
-IMAGE_HEIGHT = 288
-STEER_TRESHOLD = 20
-
 # Initialize PiCamera
 camera = PiCamera()
-camera.resolution = (IMAGE_WIDTH, IMAGE_HEIGHT)
-camera.framerate = 10
+camera.resolution = (cfg.FRAME_WIDTH, cfg.FRAME_HEIGHT)
+camera.framerate = 30
 camera.rotation = 180
 
 #initialize peopleTracker
 peopleTracker = CentroidTracker()
 
-# Initialize the HOG descriptor/people detector
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+def nothing(x):
+    pass
+ 
+cv2.namedWindow("Trackbars")
+ 
+# cv2.createTrackbar("B", "Trackbars", 0, 255, nothing)
+# cv2.createTrackbar("G", "Trackbars", 0, 255, nothing)
+# cv2.createTrackbar("R", "Trackbars", 0, 255, nothing)
 
+rawCapture = PiRGBArray(camera, size=(cfg.FRAME_WIDTH, cfg.FRAME_HEIGHT))
 
-
-rawCapture = PiRGBArray(camera, size=(IMAGE_WIDTH, IMAGE_HEIGHT))
-
-# movement controller parameters
-error = 0.0
-setpoint = int(IMAGE_WIDTH / 2)
+# Create and start PID controller thread
+controlThread = Thread(target = positionControl_PID)
+controlThread.start()
+print("thread started")
 
 #loop through frames continuously
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    startTime = time.time()
-
     image = frame.array
 
-    image = imutils.resize(image, width=min(400, image.shape[1]))
+    # image = imutils.resize(image, width=min(400, image.shape[1]))
 
 
-    # detect people in the image
-    # (rects, weights) = hog.detectMultiScale(image, winStride=(6, 6),
-    # padding=(8, 8), scale=1.05)
-    (rects, weights) = hog.detectMultiScale(image, winStride=(10, 10),
-    padding=(8, 8), scale=1.1)
+    # Find object in the image
+    
+    blurred = cv2.GaussianBlur(image, (5, 5), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV) # convert picture from BGR to HSV color format
 
-    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+    # Optional trackbars left for determining threshold 'live' if current is not working
+    # B = cv2.getTrackbarPos("B", "Trackbars")
+    # G = cv2.getTrackbarPos("G", "Trackbars")
+    # R = cv2.getTrackbarPos("R", "Trackbars")
+    B = 95
+    G = 0
+    R = 225
 
+    color = np.uint8([[[B, G, R]]])
+    hsvColor = cv2.cvtColor(color,cv2.COLOR_BGR2HSV)
+    lowerLimit = np.uint8([hsvColor[0][0][0]-10, 150,150])
+    upperLimit = np.uint8([hsvColor[0][0][0]+10,255,255])
 
-    # draw the final bounding boxes
-    for (startX, startY, endX, endY) in pick:
-        cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
+    # Apply Filters START
+    kernel = np.ones((15,15),np.uint8)
+    mask = cv2.inRange(hsv, lowerLimit, upperLimit)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    cv2.imshow("mask", mask)
+    # Apply Filters END
+
+    # Get object contours START
+    edged = cv2.Canny(mask, 35, 125)
+    contours = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+    # Get countours END
+    rects = []
+    objects = OrderedDict()
+    contoursSorted = sorted(contours, key=cv2.contourArea, reverse=True)
+    if len(contoursSorted) > 2 :
+        contoursSorted = contoursSorted[:2]
+
+    if len(contours) != 0 :
+        i = 0
+        for contour in contoursSorted:
+
+            x,y,w,h = cv2.boundingRect(contour)
+            print ("pixel width of object: %d" % w )
+            i += 1
+            # if (w) > 10:
+            rects.append([x,y,x+w,y+h])
+        # image = cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
         pass
-        # width = endX - startX
-        # height = endY - startY
-        # 
-    # objects = peopleTracker.update(pick)
 
-    # if bool(objects):
-    #     for (objectID, centroid) in objects.items():
-    #         # draw both the ID of the object and the centroid of the
-    #         # object on the output image
-    #         text = "ID {}".format(objectID)
-    #         cv2.putText(image, text, (centroid[0] - 10, centroid[1] - 10),
-    #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    #         cv2.circle(image, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+        rects = np.array(rects)
+        pick = non_max_suppression(rects, probs=None, overlapThresh=0.45)
+
+        # draw the final bounding boxes
+        for (startX, startY, endX, endY) in pick:
+            cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
+            pass
+
+        objects = peopleTracker.update(pick)
+
+    if bool(objects):
+        maxAreaBboxID = 0
+        prevArea = 0
+        for (objectID, centroid) in objects.items():
+            # draw both the ID of the object and the centroid of the
+            # object on the output image
+            centerX, centerY, area = centroid
+
+            print("objectID: %d, area: %d " % (objectID, area))
+            if prevArea < area :
+                maxAreaBboxID = objectID
+            prevArea = area
+            text = "ID {}".format(objectID)
+            cv2.putText(image, text, (centerX - 10, centerY - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.circle(image, (centerX, centerY), 4, (0, 255, 0), -1)
+
+        print("maxAreaBboxID: ", maxAreaBboxID)
+        if maxAreaBboxID in objects :
+            trackedCentroid = objects[maxAreaBboxID]
+            # print(objects[0])
+            # print(trackedCentroid)
+            centroidX = trackedCentroid[0]
+
+        startX, startY, endX, endY = pick[0]
+        # Find and display distance to the object
+
+        estimatedDistanceToObj = findCameraDistance(endX - startX)
+
+        cv2.putText(image, "%.2fcm" % (estimatedDistanceToObj),
+        (image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 3)
 
         ###### HANDLE ROBOT MOVEMENT_ START #####
+        cfg.measurement = centroidX
+        print(cfg.measurement)
 
+        if estimatedDistanceToObj > 40 :
+            cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_LEFT, dps=cfg.MAX_SPEED - cfg.correction)
+            cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_RIGHT, dps=cfg.MAX_SPEED + cfg.correction)
+        else :
+            cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_LEFT, dps=0)
+            cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_RIGHT, dps=0)
+            # cfg.MAX_SPEED -= 10
         ## get first object found centroid
         
         # followedCentroid = objects[0]
@@ -95,16 +164,15 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 
     cv2.imshow("outputImage", image)
-    endTime = time.time()
 
-    print("image processing time: ", endTime - startTime)
     # Exit if 'esc' is clicked
     # cleanup hardware
     key = cv2.waitKey(1)
     rawCapture.truncate(0)
     if key == 27:
-        GPG.reset_all()
+        cfg.GPG.reset_all()
         camera.close()
+        cfg.GPG.stop()
         break
 
 cv2.destroyAllWindows()
