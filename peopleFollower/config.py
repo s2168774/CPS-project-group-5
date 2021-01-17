db03f5829c13b380e5dd876b0f6572d20002226b
Fox <filipiukjarek@gmail.com>
2021-01-17 13:51:36 +0100

    threshold changed to recognize and follow bright pink color

--- peopleFollower/config.py
+++ peopleFollower/config.py
@@ -4,25 +4,29 @@ import easygopigo3
 FRAME_WIDTH = 640
 FRAME_HEIGHT = 480
 
-MAX_SPEED = 600
+MAX_SPEED = 200
 
 movingAverageWindowSize = 10
 distMeasurements = []
 
-# PID controller parameters START
+## PID controller parameters START ##
 previousError = 0.0
 integralError = 0.0
 loopFreq = 40
 measurement = 0
-setpoint = int(FRAME_WIDTH -  FRAME_WIDTH / 6)
+setpoint = FRAME_WIDTH / 2
 leftMotorSpeed = 0
 rightMotorSpeed = 0
 Kp = 0.2
 Kd = 0.01
-Ki = 0.1
+Ki = 0.2
 correction = 0.0
-# PID controller parameters END
+## PID controller parameters END ##
 
+## Distance from RSSI parameters START ##
+
+
+## Distance from RSSI parameters END ##
 threadStopper = Event()
 
 GPG = easygopigo3.EasyGoPiGo3() # Create an instance of the GoPiGo3 class. GPG will be the GoPiGo3 object.
