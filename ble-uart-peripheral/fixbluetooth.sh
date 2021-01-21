3ab9d11895ba85b008cbe56a9f927282e7467190
Aron B <aron-b@live.nl>
2021-01-21 13:37:14 +0100

    update installed bluez version

--- ble-uart-peripheral/fixbluetooth.sh
+++ ble-uart-peripheral/fixbluetooth.sh
@@ -3,9 +3,9 @@ sudo apt-get update
 sudo apt-get upgrade 
 mkdir ~/bluez 
 cd ~/bluez 
-wget http://www.kernel.org/pub/linux/bluetooth/bluez-5.43.tar.xz 
-tar xvf bluez-5.43.tar.xz 
-cd bluez-5.43/ 
+wget http://www.kernel.org/pub/linux/bluetooth/bluez-5.55.tar.xz 
+tar xvf bluez-5.55.tar.xz 
+cd bluez-5.55/ 
 sudo apt-get install -y libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev 
 ./configure 
 sudo make 
