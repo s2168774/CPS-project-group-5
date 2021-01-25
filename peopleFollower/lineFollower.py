caf2fdb040c8237a193f51303d2895d21daca7d8
Fox <filipiukjarek@gmail.com>
2021-01-25 19:04:04 +0100

    Initial code repair

--- peopleFollower/lineFollower.py
+++ peopleFollower/lineFollower.py
@@ -11,215 +11,191 @@ from threading import Thread
 from collections import OrderedDict
 from picamera.array import PiRGBArray
 
-from utils import findCameraDistance, horizontalPositionControl_PID, distanceControl_PID, findRssiDistance, rectArea, cameraInit, movingAverage
+from utils import findCameraDistance, horizontalPositionControl_PID, distanceControl_PID, findRssiDistance, rectArea, cameraInit, movingAverage, getColorLimitsFromBGR, getFilteredColorMask, getAreaSortedContours, drawBoxes, getBoundingBoxes, drawObjectCoordinates, findCenterOfBiggestBox
+
 from centroidTracker import CentroidTracker
-from imutils.object_detection import non_max_suppression
+
 from wifiScanner import WifiScanner
 
-# Initialize PiCamera
-camera = cameraInit()
-
-#initialize peopleTracker
-peopleTracker = CentroidTracker(maxDisappeared=5)
-
-# def nothing(x):
-#     pass
- 
-# cv2.namedWindow("Trackbars")
- 
-# cv2.createTrackbar("B", "Trackbars", 0, 255, nothing)
-# cv2.createTrackbar("G", "Trackbars", 0, 255, nothing)
-# cv2.createTrackbar("R", "Trackbars", 0, 255, nothing)
-
-centroidX = 0
-width = 1
-objects = OrderedDict()
-
-# Create and start PID controller thread
-horizontalPositionControlThread = Thread(target = horizontalPositionControl_PID)
-horizontalPositionControlThread.start()
-print("horizontal control thread started")
-
-# distanceControlThread = Thread(target = distanceControl_PID)
-# distanceControlThread.start()
-# print("distance control thread started")
 
 
-# # Handle Bluetooth Low Energy device scan
-# scanDelegate = ble.ScanDelegate()
-# deviceScanner = ble.DeviceScanner(scanDelegate)
-# # deviceScanner.startScan(float("inf"))
-# deviceScanner.showAvilableDevices(scanDelegate)
-# # TODO get values from scanner thread
+class LineFollower() :
 
-# wifiScanner = WifiScanner()
+    def __init__(self) :
+        # self.currentPath = np.array(cfg.color_PINK)
+        self.currentColors = []
+        self.step = 0
+        self.path = []
+        self.isNextStep = False
 
-# # deviceScannerThread = Thread(target = wifiScanner.probeRssi)
-# deviceScannerThread = Thread(target = deviceScanner.startScan)
-# deviceScannerThread.start()
-# print("device scanner thread started")
+    def updateStep(self) :
+        print("len(self.path:", len(self.path))
+        if len(self.path) > 2 and self.step < len(self.path) - 2 :
+            self.step += 1
+            self.currentColors = np.array(self.path[self.step:self.step+2])
+            print(".currentColors: ", self.currentColors, "path", self.path)
+            return True
+        else :
+            return False
 
-rawCapture = PiRGBArray(camera, size=(cfg.FRAME_WIDTH, cfg.FRAME_HEIGHT))
+    def run(self, path=[cfg.color_PINK, cfg.color_YELLOW]) :
+        self.path = path
+        self.currentColors = path[:2]
+        # Initialize PiCamera
+        camera = cameraInit()
 
-#loop through frames continuously
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    image = frame.array
-    startTime = time.time()
-    # print(" my device signal strength in dB: ", scanDelegate.rssi)
-    # image = imutils.resize(image, width=min(400, image.shape[1]))
+        #initialize peopleTracker
+        firstColorTracker = CentroidTracker(maxDisappeared=20)
+        secondColorTracker = CentroidTracker(maxDisappeared=20)
+        centroidX = 0
+        width = 1
+        # objects = OrderedDict()
 
-    # print(deviceScannerThread.is_alive())
-    # Find object in the image
-    
-    blurred = cv2.GaussianBlur(image, (5, 5), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV) # convert picture from BGR to HSV color format
+        # Create and start PID controller thread
+        horizontalPositionControlThread = Thread(target = horizontalPositionControl_PID)
+        horizontalPositionControlThread.start()
+        print("horizontal control thread started")
 
-    # Optional trackbars left for determining threshold 'live' if current is not working
-    # B = cv2.getTrackbarPos("B", "Trackbars")
-    # G = cv2.getTrackbarPos("G", "Trackbars")
-    # R = cv2.getTrackbarPos("R", "Trackbars")
-    B = 50
-    G = 10
-    R = 200
+        rawCapture = PiRGBArray(camera, size=(cfg.FRAME_WIDTH, cfg.FRAME_HEIGHT))
 
-    color = np.uint8([[[B, G, R]]])
-    hsvColor = cv2.cvtColor(color,cv2.COLOR_BGR2HSV)
-    lowerLimit = np.uint8([hsvColor[0][0][0]-10, 150,40])
-    upperLimit = np.uint8([hsvColor[0][0][0]+10,255,255])
+        # def nothing(x):
+        #     pass
+         
+        # cv2.namedWindow("Trackbars")
+         
+        # cv2.createTrackbar("B", "Trackbars", 0, 255, nothing)
+        # cv2.createTrackbar("G", "Trackbars", 0, 255, nothing)
+        # cv2.createTrackbar("R", "Trackbars", 0, 255, nothing)
+        # objectTrackers = []
+        # for x in range(4):
+        #     objectTrackers.append(CentroidTracker(maxDisappeared=5))
+        #     pass
 
-    # Apply Filters START
-    kernel = np.ones((15,15),np.uint8)
-    mask = cv2.inRange(hsv, lowerLimit, upperLimit)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    # cv2.imshow("mask", mask)
-    # Apply Filters END
-
-    # Get object contours START
-    edged = cv2.Canny(mask, 35, 125)
-    contours = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = imutils.grab_contours(contours)
-    # Get countours END
-    
-    contoursSorted = sorted(contours, key=cv2.contourArea, reverse=True)
-    if len(contoursSorted) > 1 :
-        contoursSorted = contoursSorted[:1]
-
-    rects = []
-    if len(contours) != 0 :
-        for contour in contoursSorted:
-            x,y,w,h = cv2.boundingRect(contour)
-            rects.append([x,y,x+w,y+h])
-        pass
-
-        rects = np.array(rects)
-        pick = non_max_suppression(rects, probs=None, overlapThresh=0.45)
-
-        # draw the final bounding boxes
-        for (startX, startY, endX, endY) in pick:
-            cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
-            pass
-
-        objects = peopleTracker.update(pick)
-
-    if bool(objects):
-        maxAreaBboxID = 0
-        prevArea = 0
-        eraseIDs = []
-        for (objectID, centroid) in objects.items():
-            # draw both the ID of the object and the centroid of the
-            # object on the output image
-            centerX, centerY, area, width = centroid
-            if area > 100 :
-                # print("objectID: %d, area: %d " % (objectID, area))
-                if prevArea < area :
-                    maxAreaBboxID = objectID
-                prevArea = area
-                text = "ID {}".format(objectID)
-                cv2.putText(image, text, (centerX - 10, centerY - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                cv2.circle(image, (centerX, centerY), 4, (0, 255, 0), -1)
-            # else:
-            #     eraseIDs.append(objectID.copy())
-        # for ID in eraseIDs :
-        #     del objects[ID]
-        # print("maxAreaBboxID: ", maxAreaBboxID)
-        if maxAreaBboxID in objects :
-            trackedCentroid = objects[maxAreaBboxID]
-            centroidX = trackedCentroid[0]
-            width = movingAverage(trackedCentroid[3], cfg.bBoxWidths, windowSize = 10)
-
-            # startX, startY, endX, endY = pick[0]
-
-            # Find and display distance to the object
-            # estimatedDistanceToObj = findCameraDistance(endX - startX)
-
-        ###### HANDLE ROBOT MOVEMENT_ START #####
-        # cfg.horizontal_measurement = centroidX # horizontal position measurement
-        cfg.horizontal_measurement = movingAverage(centroidX, cfg.horizontalPositions, windowSize=2) # horizontal position 
-
-        # print("hor_meas", cfg.horizontal_measurement, "centroidX : ", centroidX)
-        # cfg.distance_measurement = movingAverage(findRssiDistance(cfg.rssi), cfg.distanceMeasurements, windowSize=3)
-        # print("Estimated distance to phone %d:" % (cfg.distance_measurement))
-
-        # cfg.distance_measurement = movingAverage(findCameraDistance(width), cfg.distanceMeasurements, windowSize=5)
-        # print("Estimated distance to phone %d:" % (cfg.distance_measurement))
+        # coloredObjects = OrderedDict()
+        # for x in range(4):
+        #     coloredObjects[x] = OrderedDict()
+        #     pass
+        # colorLimits = np.zeros((4, 3), dtype="uint8")
 
 
+        #loop through frames continuously
+        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+            image = frame.array
+            startTime = time.time()
 
-        # cv2.putText(image, "%.2fcm" % (cfg.distance_measurement),
-        # (image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 3)
+            blurred = cv2.GaussianBlur(image, (5, 5), 0)
+            hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV) # convert picture from BGR to HSV color format
+            # hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # convert picture 
 
-        # cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_LEFT, dps=cfg.MAX_SPEED - int(cfg.horizontal_correction + cfg.distance_correction))
-        # cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_RIGHT, dps=cfg.MAX_SPEED + int(cfg.horizontal_correction + cfg.distance_correction))
+            # for i in range(len(coloredObjects)):
+            #     coloredObjects[self.currentColors[i]] = objectTrackers[self.currentColors[i]].update()
+                # find pink line
 
-        # cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_LEFT, dps=cfg.MAX_SPEED - int(cfg.distance_correction) - int(cfg.horizontal_correction))
-        # cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_RIGHT, dps=cfg.MAX_SPEED - int(cfg.distance_correction) + int(cfg.horizontal_correction))
-        
-        cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_LEFT, dps=cfg.MAX_SPEED - int(cfg.horizontal_correction))
-        cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_RIGHT, dps=cfg.MAX_SPEED + int(cfg.horizontal_correction))
-        print("distance correction: ", cfg.distance_correction)
-        print("horizontal_correction: ", cfg.horizontal_correction)
 
-        # if cfg.distance_measurement > 40 :
-        #     cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_LEFT, dps=cfg.MAX_SPEED - cfg.horizontal_correction)
-        #     cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_RIGHT, dps=cfg.MAX_SPEED + cfg.horizontal_correction)
-        # else :
-        #     cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_LEFT, dps=0)
-        #     cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_RIGHT, dps=0)
-            # cfg.MAX_SPEED -= 10
-        ## get first object found centroid
-        
-        # followedCentroid = objects[0]
-        # followedCentroidX, followedCentroidY = followedCentroid
-        # imageMidX = IMAGE_WIDTH / 2
+            # B = 50  #blue 0..255
+            # G = 10  #green 0..255
+            # R = 200 #red 0..255
 
-        # # movement rules: stay within STEER_TRESHOLD
-        # if (followedCentroidX < (imageMidX + STEER_TRESHOLD)) and (followedCentroidX > (imageMidX - STEER_TRESHOLD)) :
-        #     GPG.steer(100, 100)
-        # elif followedCentroidX > (imageMidX + STEER_TRESHOLD):
-        #     GPG.steer(20, 100)
-        # else :
-        #     GPG.steer(100, 20)
-        # ###### HANDLE ROBOT MOVEMENT_ END #####
-    else:
-        cfg.GPG.stop()
+            # Optional trackbars left for determining threshold 'live' if current is not working
+            # B = cv2.getTrackbarPos("B", "Trackbars")
+            # G = cv2.getTrackbarPos("G", "Trackbars")
+            # R = cv2.getTrackbarPos("R", "Trackbars")
+            # lowerLimitPink, upperLimitPink = getColorLimitsFromBGR(50, 10, 200)
+            # lowerLimitYellow, upperLimitYellow = getColorLimitsFromBGR(77, 147, 192)
+            
+            
+            maskFirst = getFilteredColorMask(hsv, cfg.colorLimitsDict.lower(self.currentColors[0]), cfg.colorLimitsDict.upper(self.currentColors[0]))
+            maskSecond = getFilteredColorMask(hsv, cfg.colorLimitsDict.lower(self.currentColors[1]), cfg.colorLimitsDict.upper(self.currentColors[1]))
+            # Apply Filters END
+            # cv2.imshow("mask pink", maskFirst)
+            # pinkContoursSorted = getAreaSortedContours(maskPink)
+            # yellowContoursSorted = getAreaSortedContours(maskYellow)
 
-    cv2.imshow("outputImage", image)
-    endTime = time.time()
-    print("loopTime: ", endTime - startTime)
-    # Exit if 'esc' is clicked
-    # cleanup hardware
-    key = cv2.waitKey(1)
-    rawCapture.truncate(0)
-    if key == 27:
-        horizontalPositionControlThread.join()
-        distanceControlThread.join()
-        deviceScannerThread.join()
-        cfg.GPG.reset_all()
-        camera.close()
-        cfg.GPG.stop()
-        break
+            firstContoursSorted = getAreaSortedContours(maskFirst)
+            secondContoursSorted = getAreaSortedContours(maskSecond)
+            # print("lineFollower.py: contours len: ", len(firstContoursSorted))
+            cv2.drawContours(image, firstContoursSorted, -1, (0, 255, 0), 3)
+            # cv2.imshow("with contours", image)
 
-cv2.destroyAllWindows()
+            # cv2.imshow("contours pink", firstContoursSorted)
 
+            # pinkBoundingBoxes = getBoundingBoxes(pinkContoursSorted)
+            # yellowBoundingBoxes = getBoundingBoxes(yellowContoursSorted)
+            firstBoundingBoxes = getBoundingBoxes(firstContoursSorted)
+            secondBoundingBoxes = getBoundingBoxes(secondContoursSorted)
+
+            drawBoxes(image, firstBoundingBoxes)
+            drawBoxes(image, secondBoundingBoxes)
+            firstColorObjects = firstColorTracker.update(firstBoundingBoxes)
+            secondColorObjects = secondColorTracker.update(secondBoundingBoxes)
+            
+
+            drawObjectCoordinates(image, firstColorObjects)
+            drawObjectCoordinates(image, secondColorObjects)
+
+            #process pink Objects
+            if bool(firstColorObjects) and bool(secondColorObjects) :
+                if not self.isNextStep :
+                    self.isNextStep = self.updateStep()
+                    secondColorCenterX = findCenterOfBiggestBox(secondColorObjects)
+                    cfg.horizontal_measurement = movingAverage(secondColorCenterX, cfg.horizontalPositions, windowSize=2) # horizontal 
+                else:
+                    firstColorCenterX = findCenterOfBiggestBox(firstColorObjects)
+                    cfg.horizontal_measurement = movingAverage(firstColorCenterX, cfg.horizontalPositions, windowSize=2) # horizontal 
+                
+                cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_LEFT, dps=int(cfg.MAX_SPEED * 0.75) - int(cfg.horizontal_correction))
+                cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_RIGHT, dps=int(cfg.MAX_SPEED * 0.75) + int(cfg.horizontal_correction))
+             
+                print("Both colors detected!!")
+
+            elif bool(firstColorObjects) and not bool(secondColorObjects) :
+                if self.isNextStep :
+                    self.isNextStep = False
+                firstColorCenterX = findCenterOfBiggestBox(firstColorObjects)
+                cfg.horizontal_measurement = movingAverage(firstColorCenterX, cfg.horizontalPositions, windowSize=2) # horizontal position 
+                cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_LEFT, dps=cfg.MAX_SPEED - int(cfg.horizontal_correction))
+                cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_RIGHT, dps=cfg.MAX_SPEED + int(cfg.horizontal_correction))
+            # elif bool(firstColorObjects) and bool(secondColorObjects) :
+            #     self.step += 1
+            #     cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_LEFT, dps=0)
+            #     cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_RIGHT, dps=0)
+            #     print("STOP!")
+            elif bool(secondColorObjects) :
+                _, secondColorCenterX = findCenterOfBiggestBox(secondColorObjects)
+                cfg.horizontal_measurement = movingAverage(secondColorCenterX, cfg.horizontalPositions, windowSize=2) # horizontal position 
+                cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_LEFT, dps=cfg.MAX_SPEED - int(cfg.horizontal_correction))
+                cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_RIGHT, dps=cfg.MAX_SPEED + int(cfg.horizontal_correction))
+                
+            elif not bool(firstColorObjects) and not bool(secondColorObjects) :
+                cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_LEFT, dps=1)
+                cfg.GPG.set_motor_dps(cfg.GPG.MOTOR_RIGHT, dps=1)
+                print("STOP!")
+            
+            # print("distance correction: ", cfg.distance_correction)
+            # print("horizontal_correction: ", cfg.horizontal_correction)
+
+                # ###### HANDLE ROBOT MOVEMENT_ END #####
+            cv2.imshow("outputImage", image)
+            endTime = time.time()
+            print("loopTime: ", endTime - startTime)
+            # Exit if 'esc' is clicked
+            # cleanup hardware
+            key = cv2.waitKey(1)
+            rawCapture.truncate(0)
+            if key == 27:
+                cfg.threadStopper.set()
+                horizontalPositionControlThread.join()
+                # distanceControlThread.join()
+                # deviceScannerThread.join()
+                cfg.GPG.reset_all()
+                camera.close()
+                cfg.GPG.stop()
+                break
+
+        cv2.destroyAllWindows()
+
+def main () :
+    lineFollower()
+
+if __name__== "__main__":
+    main()
---
+++ peopleFollower/runRobotModes.py
@@ -0,0 +1,68 @@
+from lineFollower import LineFollower
+from peopleDetection import personFollower
+from threading import Thread
+import config as cfg
+
+mode = 1# getBluetoothCommand()
+
+isLineFollowerRunning = False
+isPeopleDetectionRunning = False
+MODE_EXIT = 0
+MODE_LINE_FOLLOWER = 1
+MODE_PERSON_FOLLOWER = 2
+STOP_CURRENT_MODE = 3
+
+lineFollowerThread = Thread()
+personFollowerThread = Thread()
+
+lineFollower = LineFollower()
+
+while True : #not cfg.threadStopper.is_set() :
+##
+## SCAN FOR BLUETOOTH INPUT
+##
+
+
+	if mode == MODE_LINE_FOLLOWER and not isLineFollowerRunning :
+		myPath = [cfg.color_PINK, cfg.color_YELLOW, cfg.color_PINK, cfg.color_YELLOW, cfg.color_PINK]
+		print("START line following")
+		cfg.horizontal_loopFreq = 8
+		cfg.MAX_SPEED = 100
+		cfg.horizontal_Kp =0.15 #0.23
+		cfg.horizontal_Kd = 0.4#12.0
+		cfg.horizontal_Ki = 0.0001#0.000022
+		isLineFollowerRunning=True
+		lineFollowerThread(target=lineFollower.run(myPath))
+		lineFollowerThread.start()
+	elif mode == MODE_PERSON_FOLLOWER and not isPeopleDetectionRunning:
+		print("START following person")
+		# parameters of the PID controller
+		cfg.horizontal_loopFreq = 9
+		cfg.MAX_SPEED = 400
+		cfg.horizontal_Kp = 0.7
+		cfg.horizontal_Kd = 0.7
+		cfg.horizontal_Ki = 0.0001#02#001#0.000022
+		cfg.distance_setpoint = 130
+		cfg.distance_Kp = 8.0
+		cfg.distance_Kd = 12.0
+		cfg.distance_Ki = 0.0#1
+		personFollowerThread(target=personFollower())
+		personFollowerThread.start()
+	elif mode == STOP_CURRENT_MODE:
+		cfg.threadStopper.set()
+		isLineFollowerRunning = False
+		isPeopleDetectionRunning = False
+		personFollowerThread.join()
+		lineFollowerThread.join()
+	elif mode == MODE_EXIT:
+		cfg.threadStopper.set()
+		if personFollowerThread.is_alive() :
+			personFollowerThread.join()
+		if lineFollowerThread.is_alive() :
+			lineFollowerThread.join()
+		break;
+	else :
+		print("Avilable modes:")
+		print("\tLINE FOLLOWER (MODE_LINE_FOLLOWER = 0")
+		print("\tLINE FOLLOWER (MODE_PERSON_FOLLOWER = 1")
+
