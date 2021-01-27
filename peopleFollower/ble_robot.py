d633991630bdc472c46035c8ba61ccf57934bdcf
Aron B <aron-b@live.nl>
2021-01-27 14:06:24 +0100

    removed cruft from ble_robot

--- peopleFollower/ble_robot.py
+++ peopleFollower/ble_robot.py
@@ -155,11 +155,6 @@ def find_adapter(bus):
         print('Skip adapter:', o)
     return None
 
-# def run():
-#     while True:
-#         print("running...")
-#         time.sleep(1)
-
 def main():
     global mainloop
 
@@ -180,8 +175,6 @@ def main():
     ad_manager = dbus.Interface(bus.get_object(BLUEZ_SERVICE_NAME, adapter),
                                 LE_ADVERTISING_MANAGER_IFACE)
 
-    #app = UartApplication(bus)
-    #adv = UartAdvertisement(bus, 0)
     app = RobotApplication(bus)
     adv = RobotAdvertisement(bus, 0)
 
@@ -193,9 +186,6 @@ def main():
     ad_manager.RegisterAdvertisement(adv.get_path(), {},
                                      reply_handler=register_ad_cb,
                                      error_handler=register_ad_error_cb)
-
-    control_thread = Thread(target=runRobotModes.main, daemon=True)
-    control_thread.start()
 
     try:
         mainloop.run()
